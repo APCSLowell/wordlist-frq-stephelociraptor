99ac233c160650bdc983c9157db2fc102c616a38
stephelociraptor <139821295+stephelociraptor@users.noreply.github.com>
2024-04-03 09:29:50 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,25 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    for (int i = 0; i < myList.size(); i++){
+      if (myList.get(i).length() == len){
+        count++;
+      }
+    }
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
+    int i = 0; 
+    while (i < myList.size()){
+      if (myList.get(i).length() == len){
+
+        myList.remove(i);
+      }
+      else {
+        i++;
+      }
 
   }
 }
