f9d2879e984bec5a7d7677a4e121f41c6ed5491e
stephelociraptor <139821295+stephelociraptor@users.noreply.github.com>
2024-04-03 09:38:33 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,6 +11,7 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
+    int count = 0; 
     for (int i = 0; i < myList.size(); i++){
       if (myList.get(i).length() == len){
         count++;
